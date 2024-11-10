cbc186d3c83cb5d8ce60027ef7952287d3eb15aa
farhan <mfarhan125@hotmail.com>
2024-11-10 23:39:02 +0500

    first commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div id='about' className='container pt-32'>
         <h2 className='text-4xl md:text-5xl'>About me</h2>
-        <p className='text-black pt-4'>As a student at the Governor's Initiative for Artificial Intelligence and Computing (GIAIC), I am specializing in Artificial Intelligence, Web 3.0, and Metaverse technologies. With a deep-seated commitment to technological advancement, I actively pursue professional development opportunities to remain current with emerging innovations in these rapidly evolving fields.</p>
+        <p className='text-black pt-4'>As a student at the Governors Initiative for Artificial Intelligence and Computing (GIAIC), I am specializing in Artificial Intelligence, Web 3.0, and Metaverse technologies. With a deep-seated commitment to technological advancement, I actively pursue professional development opportunities to remain current with emerging innovations in these rapidly evolving fields.</p>
       </div>
   )
 }
